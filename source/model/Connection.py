e4b2b769560e341cfa6326d2080ffc98537b9e5c
cyakisan <xyffard@outlook.com>
2026-06-05 14:23:51 +0200

    add: zone and connection class

--- source/model/Connection.py
+++ source/model/Connection.py
@@ -1,25 +1,31 @@
+from . import Zone
+
+
 class Connection:
-    def __init__(self, zones: tuple[str, str], max_link_capacity=None):
-        self.zones = zones
+    def __init__(self, zones: tuple[Zone, Zone], max_link_capacity=None):
+        self.zones = list(zones)
         self.max_link_capacity = max_link_capacity
         self.drones = []
 
-    def accessible(self, drone_zone):
+    def _get_destination(self, drone_zone):
+        if drone_zone == self.zones[0].name:
+            return self.zones[1]
+        elif drone_zone == self.zones[1].name:
+            return self.zones[0]
+        else:
+            raise ValueError("Error: drone_zone is not any of the two linked "
+                             "connections.")
+
+    def _accessible(self, drone_zone):
         if (self.max_link_capacity is not None and
-           self.drones == self.max_link_capacity or
+           len(self.drones) == self.max_link_capacity or
            drone_zone not in self.zones):
             return False
         else:
             return True
 
-    def drone_entering(self, drone):
-        if not self.accessible(drone.place):
+    def drone_passing_through(self, drone):
+        if not self._accessible(drone.place.name):
             raise ValueError("Error: this connection is not accessible")
         else:
-            self.drones.append((drone, self._get_destination(drone.place)))
-
-    def _get_destination(self, drone_zone):
-        if self.zones[0] == drone_zone:
-            return self.zones[1]
-        else:
-            return self.zones[0]
+            self._get_destination(drone.place).drone_arrival(drone)
